a5a7569828867bb6b1ed435f00369da631a15019
KUNAL KUMAR <86647758+kunal042@users.noreply.github.com>
2024-02-11 05:20:24 +0000

    operators

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -1,12 +1,12 @@
 let score = undefined
 
-console.log(typeof score); 
-console.log(typeof (score)); 
+// console.log(typeof score); 
+// console.log(typeof (score)); 
 
 let valueInNumber = Number(score)
 
-console.log(typeof (valueInNumber)); 
-console.log(valueInNumber);
+// console.log(typeof (valueInNumber)); 
+// console.log(valueInNumber);
 
 
 /*
@@ -23,7 +23,7 @@ let loggedin = 0
 
 let booleanIsLoggedIn = Boolean(loggedin)
 
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /*
 
@@ -32,3 +32,51 @@ console.log(booleanIsLoggedIn);
 "hitesh" => true
 
 */
+
+// ************************** Operations *******************************
+
+let value = 3
+
+let negvalue = -value
+
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+
+let str1 = "hello "
+let str2 = "Kunal"
+let str3 = str1 + str2 
+
+// console.log(str3);
+
+// console.log("2" +1);
+// console.log(2 +"1");
+// console.log(2 +"1");
+// console.log("1" + 2 + 2);
+// console.log(2+2 +"1");
+
+
+// console.log(+true);
+// console.log(-true);
+// console.log(+false);
+// console.log(-false);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter);
+console.log(num1, num2, num3);
+
+
+
+
+
