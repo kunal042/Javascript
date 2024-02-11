d70c391e447bb5bc7f69362bfb65f09784923c13
KUNAL KUMAR <86647758+kunal042@users.noreply.github.com>
2024-02-11 04:55:23 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,34 @@
+let score = undefined
+
+console.log(typeof score); 
+console.log(typeof (score)); 
+
+let valueInNumber = Number(score)
+
+console.log(typeof (valueInNumber)); 
+console.log(valueInNumber);
+
+
+/*
+change in Number
+
+"330"  => 33
+"33abc"  ==> NaN
+"kunal"  => NaN
+null  => NaN
+undefine => undefine
+*/
+
+let loggedin = 0
+
+let booleanIsLoggedIn = Boolean(loggedin)
+
+console.log(booleanIsLoggedIn);
+
+/*
+
+1 => True
+"" => false
+"hitesh" => true
+
+*/
