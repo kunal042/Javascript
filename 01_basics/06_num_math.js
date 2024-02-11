142577e908aeba87be85300171c7aefa46eafe80
KUNAL KUMAR <86647758+kunal042@users.noreply.github.com>
2024-02-11 15:53:59 +0000

    num and maths

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,37 @@
+const score = 400
+const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+
+const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(5))
+
+const hundred = 1000000000
+
+// console.log(hundred.toLocaleString("en-IN"));
+
+// ++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.566));
+// console.log(Math.ceil(4.566));
+// console.log(Math.floor(4.566));
+// console.log(Math.min(4,3,2,1));
+// console.log(Math.max(4,3,2,1,8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const max = 20
+const min = 10
+
+console.log(Math.floor( Math.random() * (max - min + 1 ))  + min );
