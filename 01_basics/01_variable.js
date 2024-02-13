f6edf190851dde7f2a8dc87c55322e28a7a36314
kunal kumar <k1509kunal@gmail.com>
2024-02-13 23:02:32 +0530

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,5 +1,17 @@
-console.log("Kunal")
+var a = 10
+var b = "kunal"
+var c = 33.45
+var d = 1234234567876543n
+var e =  null
+var f = undefined
+var g = true
 
-var x = "kunal"
 
-console.log(x);
+
+console.log( typeof a);
+console.log( typeof(b));
+console.log( typeof(c));
+console.log( typeof(d));
+console.log( typeof(e));
+console.log( typeof(f));
+console.log( typeof(g));
