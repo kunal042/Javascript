a38c6aa57ce77bea482228c635bd6c2ed82a7e4f
kunal kumar <k1509kunal@gmail.com>
2024-02-20 09:37:25 +0530

    string

--- practice/string.js
+++ practice/string.js
@@ -1,5 +1,76 @@
-var a = "javascript is a scripting language"
+
+var a = "javascript"
+/* 
+var a = "javascript"
 
 for (let i in a){
-    console.log();
+    // console.log(a[i]);
 }
+
+
+// console.log(a.charAt(3));
+
+console.log(a.charCodeAt());
+for (let i in a){
+    console.log(`${a[i]} =`,a.charCodeAt(i));
+}*/
+
+
+// for (let i = 0; i <100; i++){
+//     document.write(i,"=",String.fromCharCode(i), " <br>");
+// }
+
+
+// console.log(a);
+// console.log(a.indexOf("script"));
+// console.log(a.lastIndexOf("script"));
+// console.log(a.search("script"));
+// console.log(a.search("script"));
+// console.log(a.includes("script"));
+
+
+
+// console.log(a);
+// console.log(a.indexOf("php"));
+// console.log(a.lastIndexOf("php"));
+// console.log(a.search("php"));
+// console.log(a.search("php"));
+// console.log(a.includes("php"));
+
+// var b = a
+// console.log(b);
+
+
+// var a = "Ducat"
+// var b = "Education"
+// var c = "Noida"
+// var d = a+b+c
+// var e = a.concat(b,c)
+
+// console.log(d);
+// console.log(e);
+
+
+
+/* 
+var msg = prompt("Enter url or Email : ")
+if (msg.startsWith("www.")){
+    document.write(`${msg} is a web Url`)
+}
+else if(msg.endsWith("@gmail.com")){
+    document.write(`$${msg} is a gmail id` )
+}else{
+    document.write(`${msg} is a normal message.`)
+}
+*/
+
+
+var a = "javaScript is a Scripting Language"
+// console.log(a);
+// console.log(a.toLocaleLowerCase());
+// console.log(a.toLocaleUpperCase());
+// console.log(a.toUpperCase());
+
+console.log(a.substr(5,1));
+console.log(a.substring(5,10));
+console.log(a.slice(5));
