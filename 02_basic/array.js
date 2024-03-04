407ad04d0ce462950ec1ce3795d7acda16e4088d
kunal kumar <k1509kunal@gmail.com>
2024-03-04 12:36:38 +0530

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,34 @@
+const myArr = [0,1,2,3,4,5,]
+const myHeros = ["shaktiman", "krish"]
+// console.log(myArr[1]);
+
+// Arrya methods
+
+myArr.push(6)
+myArr.push(7)
+// console.log(myArr);
+
+myArr.unshift(9)
+// console.log(myArr);
+
+myArr.shift()
+// console.log(myArr);
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myArr);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,4)
+console.log(myn2);
+
+
+
+
+
+
