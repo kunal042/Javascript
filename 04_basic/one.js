95eee4f0ce789e85831f99327820c36040eea547
kunal kumar <k1509kunal@gmail.com>
2024-03-06 09:22:45 +0530

    control statement

---
+++ 04_basic/one.js
@@ -0,0 +1,23 @@
+/*
+
+// Nullish coalescing Operator (??) : null undefined
+
+*/
+
+
+let val1 ;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = undefined ?? 10 ?? 20
+
+console.log(val1);
+
+
+
+// Terniary Operators
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("Less than 80") : console.log("More than 80");
